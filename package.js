c7df0cf80bcff84a7ad56e445ce86eccf1b19be2
Robert Pitt <rpitt@centiq.co.uk>
2015-06-25 02:00:11 +0100

    Version bump

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'atomic:notify',
-  version: '1.0.1',
+  version: '1.0.2',
   // Brief, one-line summary of the package.
   summary: 'Display notifications in a notifications bar.',
   // URL to the Git repository containing the source code for this package.
